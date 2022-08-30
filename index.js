7e1ff732ce66bbeaaa73aad6478ceb96e4aece4d
Chris Ding <chrisding7@berkeley.edu>
2022-08-30 09:11:04 -0700

    commit tree ed5da312f08d191893381ff101a1a92cb9e8a921

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name : "Bob",
+    streetAddress : "123 Some Street"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {...employee, [key]:value}
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const employeeCopy = {...employee}; 
+    delete employeeCopy[key];
+
+    return employeeCopy;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+
+    return employee;
+}
